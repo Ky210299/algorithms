5793ab43c9901bc6eb02b81dc098486524ef25e2
Leo <leo@Alejandros-MacBook-Pro-2.local>
2024-08-29 13:22:12 +0200

    adding merge sort in java

---
+++ java/MergeSort.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+import java.util.Stack;
+public class MergeSort {
+
+  public static String toString(int[] arr, String title) {
+    String a = title + ": \n[";
+    String body ="";
+    String c = "]";
+    for(int e : arr) {
+      body += e + ",";
+    }
+    return a + body + c;
+  }
+
+  public static void mergeSort(int[] array) {
+    int arrLength = array.length;
+    if (arrLength < 2){
+      return;
+    }
+    int middle = arrLength / 2;
+
+    int[] ls = new int[middle];
+    int[] rs = new int[arrLength - middle];
+
+    for(int i = 0; i<middle; i++) {
+      ls[i] = array[i];
+    }
+    for(int i = middle; i<arrLength; i++) {
+      rs[i - middle] = array[i];
+    }
+    mergeSort(ls);
+    mergeSort(rs);
+    
+    // Merge
+
+     MergeSort.merge(array, ls, rs);
+
+  };
+  private static void merge(int[] arr, int[] ls, int[] rs ){
+    int lsSize = ls.length;
+    int rsSize = rs.length;
+    int i = 0, j = 0, k = 0;
+
+    while(i < lsSize &&  j < rsSize) {
+      if(ls[i] < rs[j]) {
+        arr[k] = ls[i];
+        i++;
+      } else {
+        arr[k] = rs[j];
+        j++;
+      }
+      k++;
+    }
+    while (i < lsSize) {
+        arr[k] = ls[i];
+        i++;
+        k++;
+    }
+    while (j < rsSize) {
+        arr[k] = rs[j];
+        j++;
+        k++;
+    }
+  };
+
+  public static void main(String[] args) {
+
+    int[] arr = new int[100];
+    Random rand =new Random();
+
+    for(int i = 0; i<100; i++){
+      arr[i] = rand.nextInt(100);
+  }
+    System.out.println(MergeSort.toString(arr, "Before"));
+    MergeSort.mergeSort(arr);
+    System.out.println(MergeSort.toString(arr, "After"));
+  }
+}
